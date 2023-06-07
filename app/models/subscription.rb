0ab687b740fa49c0050729dcb2bec39f1bab4cd0
Andra Helton <andrahelton@gmail.com>
2023-06-06 18:53:56 -0600

    feat: Add subscription model validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+
+  enum status: [:active, :cancelled]
+  enum frequency: [:weekly, :monthly, :yearly]
 end
