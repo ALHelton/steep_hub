4d4cf1720e6d61a740e10aa7a7143b38b189a265
Andra Helton <andrahelton@gmail.com>
2023-06-06 21:31:03 -0600

    fix: Adjusted subscription model validations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Subscription, type: :model do
 
   describe "validations" do
     it { should validate_presence_of :customer_id }
-    it { should validate_presence_of :status }
+    it { should validate_presence_of :price }
+    it { should validate_numericality_of :price }
     it { should validate_presence_of :frequency }
   end
 end
