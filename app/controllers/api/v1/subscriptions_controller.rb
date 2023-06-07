c2e39fe0b0ff8b4b7b6efae2f1a4f953b4fc5858
Andra Helton <andrahelton@gmail.com>
2023-06-06 20:41:15 -0600

    feat: Create customer subscription happy path

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,4 +2,16 @@ class Api::V1::SubscriptionsController < ApplicationController
   def index
     render json: SubscriptionSerializer.new(Customer.find(params[:customer_id]).subscriptions)
   end
+
+  def create
+    sub = Subscription.create(sub_params)
+    if sub.save
+      render json: SubscriptionSerializer.new(sub), status: 201
+    end
+  end
+end
+
+private
+def sub_params
+  params.permit(:title, :price, :frequency, :customer_id)
 end
